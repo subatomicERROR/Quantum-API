d35ff8eaf29d380f60402f331985872649524dd5
Yash Ramteke <subatomicERROR@users.noreply.huggingface.co>
2025-03-26 01:28:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -84,7 +84,3 @@ async def predict(request: PredictionRequest):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-# ✅ Start FastAPI Server
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=7860, reload=True)
